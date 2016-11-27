92e7a04719bddc8e4c64e87328f2197e01918413
mignonstyle <mignonxstyle@gmail.com>
2016-11-27 19:09:43 +0900

    modify css settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,116 +1,145 @@
 // $ npm install --save-dev gulp
 // $ npm run build
 
-var gulp         = require( 'gulp' );
-var watch        = require( 'gulp-watch' );
-var sass         = require( 'gulp-sass' );
-var csso         = require( 'gulp-csso' );
-var rename       = require( 'gulp-rename' );
+var gulp         = require( 'gulp' ),
+	sass         = require( 'gulp-sass' ),
+	sassLint     = require( 'gulp-sass-lint' ),
+	rename       = require( 'gulp-rename' ),
+	csso         = require( 'gulp-csso' );
+	// postcss      = require( 'gulp-postcss' );
+
+// var watch        = require( 'gulp-watch' );
+
+
 //var path         = require( 'path' );
 //var changed      = require( 'gulp-changed' );
-var concat       = require( 'gulp-concat' );
+// var concat       = require( 'gulp-concat' );
 //var plumber      = require( 'gulp-plumber' );
-var uglify       = require( 'gulp-uglify' );
+// var uglify       = require( 'gulp-uglify' );
 //var requireDir   = require( 'require-dir' );
-var browserSync  = require( 'browser-sync' );
+// var browserSync  = require( 'browser-sync' );
 
 // ------------------------------------------------
 // Paths setting
 // ------------------------------------------------
 
 var paths = {
-    // base paths
-    "phpSrc": "./**/*.php",
+	// base paths
+	// "phpSrc": "./**/*.php",
 
-    "scssSrc": "./src/scss/**/*.scss",
-    "scssDir": "./",
+	// scss
+	"scssSrc": "./src/scss/**/*.scss",
+	"scssDir": "./css/",
 
-    "jsSrc": "./src/js/**/*.js",
-    "jsDir": "./js/",
+	// "jsSrc": "./src/js/**/*.js",
+	// "jsDir": "./js/",
 
-    // admin widget paths
-    //"admin_scssSrc": "./src/admin/admin-widget/*.scss",
+	// admin widget paths
+	//"admin_scssSrc": "./src/admin/admin-widget/*.scss",
 
-    //"widget_scssSrc": "./src/admin/admin-widget/widget-scss/**/*.scss",
-    //"widget_scssDir": "./admin/widget/widget-css/",
+	//"widget_scssSrc": "./src/admin/admin-widget/widget-scss/**/*.scss",
+	//"widget_scssDir": "./admin/widget/widget-css/",
 
-    //"widget_jsSrc": "./src/admin/admin-widget/widget-js/**/*.js",
-    //"widget_jsDir": "./admin/widget/widget-js/",
+	//"widget_jsSrc": "./src/admin/admin-widget/widget-js/**/*.js",
+	//"widget_jsDir": "./admin/widget/widget-js/",
 }
 
 // ------------------------------------------------
 // BrowserSync
 // ------------------------------------------------
+/*
 gulp.task( 'browser-sync', function() {
-    browserSync.init( {
-        proxy : "http://vccw-test.dev/",
-        notify: true,
-        xip   : false
-    } );
+	browserSync.init( {
+		proxy : "http://vccw-test.dev/",
+		notify: true,
+		xip   : false
+	} );
 } );
 
 gulp.task( 'bs-reload', function() {
-    browserSync.reload();
+	browserSync.reload();
 } );
-
+*/
 // ------------------------------------------------
 // Sass Tasks
 // ------------------------------------------------
 gulp.task( 'scss', function() {
-    return gulp.src( paths.scssSrc )
-        .pipe( sass() )
-    .pipe( gulp.dest( paths.scssDir ) )
-	/*
-    .pipe( rename( {
-        suffix: '.min'
-    } ) )
-	*/
-    //.pipe( csso() )
-    //.pipe( gulp.dest( paths.scssDir ) );
+	return gulp.src( paths.scssSrc )
+		.pipe( sassLint() )
+		.pipe( sassLint.format() )
+		.pipe( sassLint.failOnError() )
+
+		.pipe( sass() )
+		.pipe( gulp.dest( paths.scssDir ) )
+
+		// add minify
+		.pipe( rename( {
+			suffix: '.min'
+		} ) )
+		.pipe( csso() )
+		.pipe( gulp.dest( paths.scssDir ) );
 } );
 
 // ------------------------------------------------
 // JS Tasks
 // ------------------------------------------------
+/*
 gulp.task( 'js', function() {
-    return gulp.src( paths.jsSrc )
-        .pipe( concat( 'main.js' ) )
-        .pipe( gulp.dest( paths.jsDir ) );
+	return gulp.src( paths.jsSrc )
+		.pipe( concat( 'main.js' ) )
+		.pipe( gulp.dest( paths.jsDir ) );
 } );
 
 gulp.task( 'js-min', ['js'], function() {
-    return gulp.src( paths.jsSrc )
-        .pipe( uglify( {preserveComments: 'license'} ) )
-        .pipe( concat( 'main.min.js' ) )
-        .pipe( gulp.dest( paths.jsDir ) );
+	return gulp.src( paths.jsSrc )
+		.pipe( uglify( {preserveComments: 'license'} ) )
+		.pipe( concat( 'main.min.js' ) )
+		.pipe( gulp.dest( paths.jsDir ) );
 } );
-
+*/
 // ------------------------------------------------
 // Gulp Tasks
 // ------------------------------------------------
+/*
 gulp.task( 'watch', [
-    'scss',
-    'js',
-    'js-min',
-    'browser-sync',
-    'bs-reload'
+	'scss',
+	'js',
+	'js-min',
+	'browser-sync',
+	'bs-reload'
 ], function() {
-    gulp.watch( [paths.scssSrc], ['scss'] );
-    gulp.watch( [paths.jsSrc], ['js', 'js-min'] );
+	gulp.watch( [paths.scssSrc], ['scss'] );
+	gulp.watch( [paths.jsSrc], ['js', 'js-min'] );
 
-    gulp.watch( [
-        paths.phpSrc,
-        paths.scssSrc,
-        paths.jsSrc,
-     ], function() {
-        browserSync.reload();
-    } );
+	gulp.watch( [
+		paths.phpSrc,
+		paths.scssSrc,
+		paths.jsSrc,
+	 ], function() {
+		browserSync.reload();
+	} );
 } );
-
+*/
+gulp.task( 'default', function() {
+	return gulp.watch( [paths.scssSrc], ['scss'] );
+} );
+/*
 gulp.task( 'default', [
-    'scss',
-    'js',
-    'js-min',
-    //'watch'
-    ], function() {
+	'scss',
+	'js',
+	'js-min',
+	//'watch'
+	], function() {
 } );
+*/
+// ------------------------------------------------
+// Test Tasks
+// ------------------------------------------------
+/*
+gulp.task( 'test:sass', function() {
+	return gulp.src( paths.scssSrc )
+		.pipe( sassLint() )
+		.pipe( sassLint.format() )
+		.pipe( sassLint.failOnError() )
+} );
+*/
